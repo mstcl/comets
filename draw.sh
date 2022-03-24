6e730782110da385b9ed6dc209936098f6ce06f0
lckdscl <lckdscl+github@protonmail.com>
2022-03-24 16:40:33 +0000

    Final third simulation

--- draw.sh
+++ draw.sh
@@ -5,8 +5,8 @@ for f in *.?????.ras
 do
 	convert $f $f.jpg
 done
-cat *.jpg > movie.mjpeg
-ffmpeg -i movie.mjpeg -qscale 0 movie.mpeg
-vlc movie.mpeg
-rm -f movie.mjpeg
+cat *.jpg > crash.mjpeg
+ffmpeg -i crash.mjpeg -qscale 0 crash.mpeg
+vlc crash.mpeg
+rm -f crash.mjpeg
 rm -f *.ras*
