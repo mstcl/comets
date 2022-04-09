4ffe3a71bf6b78234ad35d8391e9073948fed03a
lckdscl <lckdscl+github@protonmail.com>
2022-04-09 16:06:24 +0100

    Fix: run less tasks async to reduce read/write errors

--- scripts/run_in_parallel.py
+++ scripts/run_in_parallel.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 """Change into 5 directories at a time, run automate.sh in each directory"""
 
-from asyncio.subprocess import PIPE, STDOUT
+from asyncio.subprocess import PIPE
 import time
 import asyncio
-import numpy as np
 import warnings
+import numpy as np
 
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 
@@ -26,15 +26,17 @@ async def main():
     """
     Driver code
     """
-    start_density = 300
-    end_density = 600
-    step = 5
-    values = np.linspace(
-        start_density, end_density, int((end_density - start_density)/step) + 1, endpoint=True
-    )
-    commands = [run(f"./scripts/automate_from_parent.sh {int(val)}") for val in values]
-    for task in asyncio.as_completed(commands):
-        await task
+    groups = [(300,400), (405,500), (505,600)]
+    for group in groups:
+        start_density = group[0]
+        end_density = group[1]
+        step = 5
+        values = np.linspace(
+            start_density, end_density, int((end_density - start_density)/step) + 1, endpoint=True
+        )
+        commands = [run(f"./scripts/automate_from_parent.sh {int(val)}") for val in values]
+        for task in asyncio.as_completed(commands):
+            await task
 
 
 if __name__ == "__main__":
