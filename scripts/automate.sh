a86b749a79c258c0fab5fb581b9d87b9002412d9
lckdscl <lckdscl+github@protonmail.com>
2022-04-09 16:05:28 +0100

    Edit: Formatting

--- scripts/automate.sh
+++ scripts/automate.sh
@@ -5,28 +5,28 @@
 
 ../scripts/rpg > sl9_stats.txt
 echo "• rpg run & sl9_stats.txt generated."
-sleep 0.1
 
+sleep 0.1
 ../scripts/get_dynamical_time.py > /dev/null 2>&1
 echo "• dDelta in ss.par updated."
-sleep 0.1
 
+sleep 0.1
 ../scripts/rpx.sh > /dev/null 2>&1
 echo "• rubber pile given initial conditions."
-sleep 0.1
 
+sleep 0.1
 ../scripts/pkdgrav ss.par > /dev/null 2>&1
 echo "• pkdgrav integration complete."
-sleep 0.1
 
+sleep 0.1
 ../scripts/batch_convert_ss2bt.sh > /dev/null 2>&1
 echo "• non-bin files converted."
-sleep 0.1
 
+sleep 0.1
 ../scripts/draw.sh > /dev/null 2>&1
 echo "• movie generated."
-sleep 0.1
 
+sleep 1
 ../scripts/plot_and_analyse_bt.py
 echo "• metadata extracted."
 
