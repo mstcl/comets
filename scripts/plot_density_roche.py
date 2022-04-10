5f7669fe7c3487c5cb1fca3e7ee0aa21186522d5
lckdscl <lckdscl+github@protonmail.com>
2022-04-10 03:39:25 +0100

    Fixing conflict

--- scripts/plot_density_roche.py
+++ scripts/plot_density_roche.py
@@ -20,13 +20,19 @@ def main():
     density = data[1][0:]
     roche_limit = data[2][0:]
     errors = data[3][0:]
+    perijove = data[4][0:]
+    density, roche_limit, errors = zip(*sorted(zip(density, roche_limit, errors)))
+    density = np.array(density)
+    dummy_density = np.linspace(100,1000,100)
 
     rho_jupiter = 1326
     r_jupiter = 71492000
 
-    roche_estimated_rigid = r_jupiter * (2 * (rho_jupiter / density)) ** (1 / 3) / 1000
-    roche_estimated_fluid = r_jupiter * 2.4823 * (rho_jupiter / density) ** (1 / 3) / 1000
-
+    roche_estimated_rigid = r_jupiter * (2 * (rho_jupiter / dummy_density)) ** (1 / 3) / 1000
+    roche_estimated_fluid = (
+        r_jupiter * 2.4823 * (rho_jupiter / dummy_density) ** (1 / 3) / 1000
+    )
+    plt.clf()
     plt.ylabel(r"Disruption distance at threshold separation / km", fontsize=13)
     plt.ticklabel_format(axis="y", style="sci", scilimits=(0, 0))
     plt.xlabel(r"Comet's bulk density / kg/m$^3$", fontsize=13)
@@ -40,14 +46,42 @@ def main():
         yerr=errors,
         color="mediumpurple",
         marker="x",
-        label="estimated",
+        label="estimated disrupted distance",
         ls="None",
-        ms=2,
+        ms=1,
         capsize=2,
     )
-    # plt.plot(density, roche_estimated_rigid, 'g*', label="rigid")
-    # plt.plot(density, roche_estimated_fluid, 'y*', label="fluid")
+    plt.plot(dummy_density, roche_estimated_rigid, color="g", label="rigid", alpha=0.4)
+    plt.plot(
+        dummy_density,
+        roche_estimated_fluid,
+        color="y",
+        label="fluid",
+        alpha=0.4,
+    )
     plt.tight_layout()
+    closest_distance = [99305.6547187192] * len(dummy_density)
+    plt.ylim([np.max(closest_distance)-2e04, np.min(roche_estimated_fluid)+4e04])
+    plt.xlim([np.min(density)-30, np.max(density)+30])
+    plt.plot(
+        dummy_density,
+        closest_distance,
+        color="k",
+        linestyle="-",
+        alpha=0.4,
+        linewidth=1,
+        label="actual perijove",
+    )
+    plt.plot(
+        density,
+        perijove,
+        color="k",
+        marker=".",
+        ls="None",
+        ms=1,
+        label="perijove",
+    )
+    plt.legend(loc="best", fontsize="small")
     plt.savefig("./roche_limit_density.png", format="png", dpi=150)
 
 
