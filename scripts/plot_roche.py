50a3cb5421370ca0f7b6d7b1c6ad8800c3022ac5
lckdscl <lckdscl+github@protonmail.com>
2022-04-10 16:48:15 +0100

    Edited: formatting and renaming vars, allow quicker un/commenting

--- scripts/plot_roche.py
+++ scripts/plot_roche.py
@@ -17,38 +17,42 @@ def main():
         data = np.array(
             [list(map(float, line.strip("\n").split(" "))) for line in file.readlines()]
         ).T
-    density = data[1][0:]
+    quantity = data[1][0:]
     roche_limit = data[2][0:]
     errors = data[3][0:]
     perijove = data[4][0:]
-    density, roche_limit, errors = zip(*sorted(zip(density, roche_limit, errors)))
-    density = np.array(density)
-    dummy_density = np.linspace(100, 1000, 100)
+    quantity, roche_limit, errors = zip(*sorted(zip(quantity, roche_limit, errors)))
+    minimum_quantity_no_disruption = quantity[np.array(roche_limit).argmin()]
+    quantity = np.array(quantity)
+    dummy_quantity = np.linspace(100, 1000, 100)
+    closest_distance = [99305.6547187192] * len(dummy_quantity)
 
     rho_jupiter = 1326
     r_jupiter = 71492000
 
     roche_estimated_rigid = (
-        r_jupiter * (2 * (rho_jupiter / dummy_density)) ** (1 / 3) / 1000
+        r_jupiter * (2 * (rho_jupiter / dummy_quantity)) ** (1 / 3) / 1000
     )
     roche_estimated_fluid = (
-        r_jupiter * 2.4823 * (rho_jupiter / dummy_density) ** (1 / 3) / 1000
+        r_jupiter * 2.4823 * (rho_jupiter / dummy_quantity) ** (1 / 3) / 1000
     )
+
+    ########################
+    #  GENERAL PLOT STUFF  #
+    ########################
+
     plt.clf()
-    plt.ylabel(r"Disruption distance at threshold separation / km", fontsize=13)
+    plt.tight_layout()
+
     plt.ticklabel_format(axis="y", style="sci", scilimits=(0, 0))
-    # plt.xlabel(r"Comet's bulk density / kg/m$^3$", fontsize=13)  # density
-    plt.xlabel(r"Comet's average radius / m", fontsize=13)
-    # plt.title(
-    #     "Effect of bulk density of a comet on its\n Roche limit (wrt Jupiter) as produced by pkdgrav",
-    #     fontsize=13,
-    # )  # density
-    plt.title(
-        "Effect of radius of a comet on its\n Roche limit (wrt Jupiter) as produced by pkdgrav",
-        fontsize=13,
-    )  # bulk semi-axes
+
+    plt.ylim([np.max(closest_distance) - 2e04, np.min(roche_estimated_fluid) + 4e04])
+    plt.xlim([np.min(quantity) - 30, np.max(quantity) + 30])
+
+    plt.ylabel(r"Disruption distance at threshold separation / km", fontsize=13)
+
     plt.errorbar(
-        density,
+        quantity,
         roche_limit,
         yerr=errors,
         color="mediumpurple",
@@ -58,29 +62,19 @@ def main():
         ms=1,
         capsize=2,
     )
-    # plt.plot(dummy_density, roche_estimated_rigid, color="g", label="rigid", alpha=0.4)  # density
-    # plt.plot(
-    #     dummy_density,
-    #     roche_estimated_fluid,
-    #     color="y",
-    #     label="fluid",
-    #     alpha=0.4,
-    # )  # density
-    plt.tight_layout()
-    closest_distance = [99305.6547187192] * len(dummy_density)
-    plt.ylim([np.max(closest_distance) - 2e04, np.min(roche_estimated_fluid) + 4e04])
-    plt.xlim([np.min(density) - 30, np.max(density) + 30])
+
     plt.plot(
-        dummy_density,
+        dummy_quantity,
         closest_distance,
         color="k",
         linestyle="-",
-        alpha=0.4,
+        alpha=0.2,
         linewidth=1,
         label="actual perijove",
     )
+
     plt.plot(
-        density,
+        quantity,
         perijove,
         color="k",
         marker=".",
@@ -88,6 +82,56 @@ def main():
         ms=1,
         label="perijove",
     )
+
+    plt.axvline(
+        minimum_quantity_no_disruption,
+        color="r",
+        linestyle="-",
+        linewidth=1,
+        alpha=0.2,
+        label="minimum distance of coalescence",
+    )
+
+    ####################
+    #  BULK SEMI-AXES  #
+    ####################
+
+    plt.xlabel(
+        r"Comet's average radius / m", fontsize=13
+    )  # uncomment for bulk semi-axes
+
+    plt.title(
+        "Effect of radius of a comet on its\n Roche limit (wrt Jupiter) as produced by pkdgrav",
+        fontsize=13,
+    )  # uncomment for bulk semi-axes
+
+    #############
+    #  DENSITY  #
+    #############
+
+    # plt.xlabel(r"Comet's bulk density / kg/m$^3$", fontsize=13)  # uncomment for density
+
+    # plt.title(
+    #     "Effect of bulk density of a comet on its\n Roche limit (wrt Jupiter) as produced by pkdgrav",
+    #     fontsize=13,
+    # )  # uncomment for density
+
+    # plt.plot(
+    #     dummy_quantity, roche_estimated_rigid, color="g", label="rigid", alpha=0.2
+    # )  # uncomment for density
+
+    # plt.plot(
+    #     dummy_quantity,
+    #     roche_estimated_fluid,
+    #     color="y",
+    #     label="fluid",
+    #     alpha=0.2,
+    # )  # uncomment for density
+
+    #######################
+    #  ADD LEGEND & SAVE  #
+    #######################
+
     plt.legend(loc="best", fontsize="small")
     plt.savefig("./roche_limit_plot.png", format="png", dpi=150)
 
