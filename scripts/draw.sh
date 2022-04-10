209a15e2c546baf5b3a5785e2a50e8459337e91d
lckdscl <lckdscl+github@protonmail.com>
2022-04-10 02:45:37 +0100

    Edit: overwrite `crash.mpeg` automatically; proper globbing formatting

--- scripts/draw.sh
+++ scripts/draw.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-../scripts/ssdraw *.?????
+../scripts/ssdraw ./*.?????
 for f in *.?????.ras
 do
-	convert $f $f.jpg
+	convert "$f" "$f.jpg"
 done
-cat *.jpg > crash.mjpeg
-ffmpeg -i crash.mjpeg -qscale 0 crash.mpeg
+cat ./*.jpg > crash.mjpeg
+ffmpeg -y -i crash.mjpeg -qscale 0 crash.mpeg # flag y will overwrite crash.mpeg automatically
 rm -f crash.mjpeg
-rm -f *.ras*
+rm -f ./*.ras*
