6e19de78ce7854a222f00a4fca2831a3746b5d02
lckdscl <lckdscl+github@protonmail.com>
2022-04-11 12:26:18 +0100

    Remove sim files

--- scripts/automate_from_parent.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-parent_dir=$PWD
-cd "$1" || exit
-../scripts/automate.sh
-cd "$parent_dir" || exit
--- scripts/batch_convert_ss2bt.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-mkdir -p bt_files
-
-for f in *.?????
-do
-	../scripts/ss2bt $f
-    mv "${f}.bt" bt_files/
-done
